daf282c60642e85bf4afbfc3b3026735876a333e
fyniteDEV <90278187+fyniteDEV@users.noreply.github.com>
2024-07-01 21:04:11 +0200

    api functional: returns user's playtime

--- server/server.js
+++ server/server.js
@@ -4,15 +4,20 @@ const http = require("node:http");
 
 const server = http.createServer();
 
+let returnText = "";
 // Listen for GET requests asking for info of a given steamId
-server.on("request", (req, res) => {
-    res.writeHead(200);
-    res.end("Request sent.");
-
+server.on("request", async (req, res) => {
+    
     const url = new URL(req.url, `http://${config.host}:${config.port}`);
     let urlParamSteamId = url.searchParams.get("steamid");
+    
+    if (urlParamSteamId != null) {
+        await getOwnedGames(urlParamSteamId);
+        res.writeHead(200);
+        res.end(returnText);
+        returnText = "";
+    }
 
-    getUserInfo(urlParamSteamId);
 })
 
 server.listen(config.port, config.host, () => {
@@ -21,32 +26,50 @@ server.listen(config.port, config.host, () => {
 
 
 // Get given steamId's owned game info from Steam Web API
-function getUserInfo(steamId) {
-    var options = {
-        'method': 'GET',
-        'hostname': 'api.steampowered.com',
-        'path': `/IPlayerService/GetOwnedGames/v0001/?key=${config.STEAM_API_KEY}&steamid=${steamId}&format=json`,
-        'headers': {
-        },
-        'maxRedirects': 20
-    };
-
-    var req = http.request(options, function (res) {
-        var chunks = [];
-
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
+function getOwnedGames(steamId) {
+    return new Promise((resolve, reject) => {
+        var options = {
+            'method': 'GET',
+            'hostname': 'api.steampowered.com',
+            'path': `/IPlayerService/GetOwnedGames/v0001/?key=${config.STEAM_API_KEY}&steamid=${steamId}&format=json`,
+            'headers': {
+            },
+            'maxRedirects': 20
+        };
+    
+        var req = http.request(options, function (res) {
+            var chunks = [];
+    
+            res.on("data", function (chunk) {
+                chunks.push(chunk);
+            });
+    
+            res.on("end", function (chunk) {
+                var body = Buffer.concat(chunks);
+                let allPlaytime = getAllPlaytimeMins(JSON.parse(body.toString()).response.games);
+                console.debug(allPlaytime);
+                returnText += allPlaytime;
+                resolve();
+            });
+    
+            res.on("error", function (error) {
+                console.error(error);
+                reject(error)
+            });
         });
+    
+        req.end();
 
-        res.on("end", function (chunk) {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
-        });
+    })
 
-        res.on("error", function (error) {
-            console.error(error);
-        });
-    });
+}
 
-    req.end();
+
+function getAllPlaytimeMins(games) {
+    let minutesPlayed = 0;
+    for (let g = 0; g < games.length; g++) {
+        minutesPlayed += games[g].playtime_forever;
+    }
+
+    return minutesPlayed;
 }
