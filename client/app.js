b803a73923d7b6810fd6c6a1ae72c2d4f986f490
fyniteDEV <90278187+fyniteDEV@users.noreply.github.com>
2024-07-16 16:19:06 +0200

    frontend - show profile info, styling

--- client/app.js
+++ client/app.js
@@ -1,10 +1,24 @@
+var profile = {};
+
 function getUserInfo() {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-            console.log(this.responseText);
+            profile = JSON.parse(this.responseText);
+
+            document.getElementById("profile").style.display = "flex";
+            document.getElementsByTagName("footer")[0].style.display = "block";
+
+            document.getElementById("avatar-img").src = profile.avatar;
+            document.getElementById("persona-name").innerHTML = profile.personaName;
+            document.getElementById("playtime").innerHTML = "All playtime: " + minsToHoursAndMins(profile.playtime);
+            document.getElementById("profile-url").href = profile.profileUrl;
+            document.getElementById("unspent-playtime").innerHTML = "Time available: " + minsToHoursAndMins(profile.playtime);
+
+
+            console.log(profile);
         }
     });
 
@@ -12,4 +26,11 @@ function getUserInfo() {
     xhr.open("GET", "http://127.0.0.1:8000?steamid=" + steamId);
 
     xhr.send();
+}
+
+function minsToHoursAndMins(mins) {
+    let hours = Math.floor(mins / 60);
+    let minutes = mins % 60;
+
+    return `${hours} hours ${minutes} minutes`
 }
